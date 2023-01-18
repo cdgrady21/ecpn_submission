4dbb87d1d33f714b8a6f76a138eb3e30c735e16d
cdgrady21 <cdgrady21@gmail.com>
2023-01-17 21:21:58 -0500

    work on individual-level stuff for appendix, new function to compare participants to controls and nonparticipants to controls, get coefs & ps.  Next run it and add output to appendix.

--- data_and_code/survey_dat/d_analysis/true_funs_ind.R
+++ data_and_code/survey_dat/d_analysis/true_funs_ind.R
@@ -2,7 +2,7 @@
 ##Want to replace all lm() with lm_lin() but lm_lin() does not allow me to vary treatment in a function.-->
 rm(list=ls())
 
-# true pval function.  Might not be necessary for panel since we have a decent number of obs, but still few clusters.
+# true pval function.  Might not be necessary for panel since we have a decent number of obs...but still few clusters.
 ## for panel, might need the covar adjust version because possible confounders.
 ## chris: should block here should be community? No, then treatment perfectly correlates with the block, washing out effect.
 true.fun1 <- function(var, tr)
@@ -66,7 +66,7 @@ true.fun1 <- function(var, tr)
 #true.fun1(var='cohes_cw', tr='bene_cw')
 
 
-########## Samii Suggestion
+########## controlling-for
 true.fun <- function(var, tr)
 {
   if(grepl("resid", var)){
@@ -263,6 +263,146 @@ strat.fun <- function(var, dat=panel.df){
   }
   else(return("Fail"))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########
+# committee vs control and non-committee vs control
+## differencing
+comp.fun1 <- function(var, nsims)
+{
+  lm1 <- lm(panel.df[,var] ~ tr_f+state, panel.df)
+  obs.coef_nonpart <- coef(lm1)[2]
+  obs.coef_part <- coef(lm1)[3]
+  
+  rand.coefs <- matrix(data=NA, nrow=nsims, ncol=2)
+  colnames(rand.coefs) <- c("nonpart", "part")
+  # I think correct thing is: (1) for treatment shuffle PSU-level treatment within state, (2) for committee shuffle committee within Community for the comm/non comparison.  To avoid committee people in non-treated areas, need to make "committee" column for "newtr" the same as committee for "treatment" before shuffling.
+  for(i in 1:nsims){
+    # randomly select for treatment PSUs within state
+    rand.nas <- sample(unique(panel.df$psu[panel.df$state %in% "nas"]), size=6)
+    rand.ben <- sample(unique(panel.df$psu[panel.df$state %in% "ben"]), size=4)
+    rand <- c(as.character(rand.nas), as.character(rand.ben))
+    rand.samp <- panel.df
+    
+    # 10p had no preselected, need to add it as 0% committee so have a committee proportion for each tr site pair
+    rand.samp[nrow(rand.samp)+1,] <- NA
+    rand.samp$community <- as.character(rand.samp$community)
+    rand.samp[nrow(rand.samp), c("treatment", "community", "psu", "committee")] <- c(1,"10.pastoralists", 10, 0)
+    rand.samp[,"newtr"] <- ifelse(rand.samp$psu %in% rand, 1, 0)
+    
+    # randomly select respondents to be committee members within newtr communities
+    committees <- droplevels(rand.samp[rand.samp$treatment %in% 1, c("committee", "community")])
+    toadd <- as.vector(prop.table(table(committees$community, committees$committee),1)[,"1"]) # prop of commit ppl in each tr community
+    tr.samp <- droplevels(rand.samp[rand.samp$newtr %in% 1,]) #simulated treatment people
+    tr.samp$newcomm <- randomizr::block_ra(blocks = tr.samp$community, block_prob = shuffle(as.numeric(toadd)))
+    rand.samp$newcomm <- tr.samp$newcomm[match(rand.samp$id_num, tr.samp$id_num)]
+    rand.samp$newcomm[is.na(rand.samp$newcomm)] <- 0 # make control ppl 0
+    rand.samp["new_tr_f"] <- droplevels(interaction(rand.samp[,"newtr"], rand.samp[,"newcomm"]))
+    
+    lm.null <- lm(rand.samp[,var] ~ new_tr_f+state, rand.samp)
+    rand.coefs[i,1] <- coef(lm.null)[2]
+    rand.coefs[i,2] <- coef(lm.null)[3]
+    
+  }
+  
+  # now it's: "how many times are these rand coefs greater than or equal to what we observed?"
+  thep_nonpart <- mean(rand.coefs[,'nonpart']>obs.coef_nonpart)
+  thep_part <- mean(rand.coefs[,'part']>obs.coef_part)
+  thedf_nonpart <- data.frame(thecoef=obs.coef_nonpart, truep=thep_nonpart)
+  thedf_part <- data.frame(thecoef=obs.coef_part, truep=thep_part)
+  rownames(thedf_nonpart) <- paste0(var, "-", "nonpart")
+  rownames(thedf_part) <- paste0(var, "-", "part")
+  thedf <- rbind(thedf_nonpart, thedf_part)
+  return(thedf)
+}
+#comp.fun1("attitude_cw",nsims=1000)
+
+# Controlling-for
+comp.fun <- function(var, nsims)
+{
+  lm1 <- lm(panel.df[,paste0(var,"_y1")] ~ tr_f+state+panel.df[,paste0(var,"_y0")], panel.df)
+  obs.coef_nonpart <- coef(lm1)[2]
+  obs.coef_part <- coef(lm1)[3]
+  
+  rand.coefs <- matrix(data=NA, nrow=nsims, ncol=2)
+  colnames(rand.coefs) <- c("nonpart", "part")
+  # I think correct thing is to: (1) for treatment shuffle PSU-level treatment within state, (2) for committee shuffle committee within Community for the comm/non comparison.  To avoid committee people in non-treated areas, need to make "committee" column for "newtr" the same as committee for "treatment" before shuffling.
+  for(i in 1:nsims){
+    # randomly select for treatment PSUs within state
+    rand.nas <- sample(unique(panel.df$psu[panel.df$state %in% "nas"]), size=6)
+    rand.ben <- sample(unique(panel.df$psu[panel.df$state %in% "ben"]), size=4)
+    rand <- c(as.character(rand.nas), as.character(rand.ben))
+    rand.samp <- panel.df
+    
+    # 10p had no preselected, need to add it as 0% committee so have a committee proportion for each tr site pair
+    rand.samp[nrow(rand.samp)+1,] <- NA
+    rand.samp$community <- as.character(rand.samp$community)
+    rand.samp[nrow(rand.samp), c("treatment", "community", "psu", "committee")] <- c(1,"10.pastoralists", 10, 0)
+    rand.samp[,"newtr"] <- ifelse(rand.samp$psu %in% rand, 1, 0)
+    
+    # randomly select respondents to be committee members within newtr communities
+    committees <- droplevels(rand.samp[rand.samp$treatment %in% 1, c("committee", "community")])
+    toadd <- as.vector(prop.table(table(committees$community, committees$committee),1)[,"1"]) # prop of commit ppl in each tr community
+    tr.samp <- droplevels(rand.samp[rand.samp$newtr %in% 1,]) #simulated treatment people
+    tr.samp$newcomm <- randomizr::block_ra(blocks = tr.samp$community, block_prob = shuffle(as.numeric(toadd)))
+    rand.samp$newcomm <- tr.samp$newcomm[match(rand.samp$id_num, tr.samp$id_num)]
+    rand.samp$newcomm[is.na(rand.samp$newcomm)] <- 0 # make control ppl 0
+    rand.samp["new_tr_f"] <- droplevels(interaction(rand.samp[,"newtr"], rand.samp[,"newcomm"]))
+    
+    lm.null <- lm(rand.samp[,var] ~ new_tr_f+state, rand.samp)
+    rand.coefs[i,1] <- coef(lm.null)[2]
+    rand.coefs[i,2] <- coef(lm.null)[3]
+    
+  }
+  
+  # now it's: "how many times are these rand coefs greater than or equal to what we observed?"
+  thep_nonpart <- mean(rand.coefs[,'nonpart']>obs.coef_nonpart)
+  thep_part <- mean(rand.coefs[,'part']>obs.coef_part)
+  thedf_nonpart <- data.frame(thecoef=obs.coef_nonpart, truep=thep_nonpart)
+  thedf_part <- data.frame(thecoef=obs.coef_part, truep=thep_part)
+  rownames(thedf_nonpart) <- paste0(var, "-", "nonpart")
+  rownames(thedf_part) <- paste0(var, "-", "part")
+  thedf <- rbind(thedf_nonpart, thedf_part)
+  return(thedf)
+}
+
+###################
+
+# controlling for or differencing?
+## if all tr_f groups balanced at baseline, controlling-for.  Otherwise, differencing.
+strat.fun <- function(var, dat=panel.df){
+  thesd <- sd(dat[[var]], na.rm=T)*.2
+  thediff_part <- abs(sort(mosaic::mean(dat[[var]]~dat$tr_f, na.rm=T))[['0.0']]-sort(mosaic::mean(dat[[var]]~dat$tr_f, na.rm=T))[['1.1']])
+  thediff_nonpart <- abs(sort(mosaic::mean(dat[[var]]~dat$tr_f, na.rm=T))[['0.0']]-sort(mosaic::mean(dat[[var]]~dat$tr_f, na.rm=T))[['1.0']])
+  thebal_part <- thediff_part-thesd
+  thebal_nonpart <- thediff_nonpart-thesd
+  if(thebal_part<0 & thebal_nonpart<0){
+    return("Controlling-for, comp.fun")
+  }
+  if(thebal_part>=0 | thebal_nonpart>=0){
+    return("Difference, comp.fun1")
+  }
+  else(return("Fail"))
+}
+strat.fun(var="contactOnly_cw_y0")
+
+
+
 
 
 #save
